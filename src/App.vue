0bd2e3f034bbd6998c90c6edd40e64dcedc0eabe
Kim Myungseong <forwarm5891@gmail.com>
2022-04-21 01:50:16 +0900

    fix: scss file directory and auto-adjust mixin

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>{{ count }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 2,
+    };
+  },
+  beforeCreate(){
+    console.log('Before Create!',this.count);
+  },
+  created() {
+    console.log('Created!',this.count);
+  },
+  beforeMount(){
+    console.log('beforeMOunt',this.count);
+    console.log(document.querySelector('h1'));
+  },
+  mounted(){
+    console.log('Mounted!',this.count);
+    console.log(document.querySelector('h1'));
+  }
+
+};
+</script>
